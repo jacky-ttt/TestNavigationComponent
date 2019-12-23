a5ef0b9e725251af52a39588141f5efaa44c1f27
Jacky Tsang <jacky.sc.tsang@gtomato.com>
2019-12-23 12:12:19 +0800

    go to fragment a using nav_graph

---
+++ app/src/main/java/com/jacky/testnavigationcomponent/FragmentA.kt
@@ -0,0 +1,28 @@
+package com.jacky.testnavigationcomponent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_a.*
+
+
+class FragmentA : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toFragmentB.setOnClickListener {
+        }
+    }
+}
